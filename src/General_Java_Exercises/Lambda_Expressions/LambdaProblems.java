2f1e7337bfe09f579a5bde1adf89d532373f7f5e
Devin Diaz <diazdevin7@outlook.com>
2024-05-25 23:06:07 -0400

    lambdas exist -> for loops DNE

--- src/General_Java_Exercises/Lambda_Expressions/LambdaProblems.java
+++ src/General_Java_Exercises/Lambda_Expressions/LambdaProblems.java
@@ -17,8 +17,11 @@ package General_Java_Exercises.Lambda_Expressions;
 import General_Java_Exercises.Lambda_Expressions.Functional_Interfaces.SumCalculator;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.LongUnaryOperator;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class LambdaProblems {
 
@@ -69,9 +72,44 @@ public class LambdaProblems {
     }
 
     // 5. Write a Java program to implement a lambda expression to sort a list of strings in alphabetical order.
-    
+    public static void sortStrings(List<String> inputs) {
+        System.out.println("Original list: " + inputs);
+        inputs.sort((str1, str2) -> str1.compareToIgnoreCase(str2));
+        System.out.println("Sorted list: " + inputs);
+    }
 
+    //6. Write a Java program to implement a lambda expression to find the average of a list of doubles.
+    public static void averageOfDoubles(List<Double> inputs) {
+        Double average = inputs.stream()
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElse(0.0);
 
+        System.out.println(average);
+    }
+
+    // 7. Write a Java program to implement a lambda expression to remove duplicates from a list of integers.
+    public static void removeDuplicates(List<Integer> inputs) {
+        List<Integer> outputs = inputs.stream()
+                .distinct()
+                .toList();
+        System.out.println(outputs);
+    }
+
+    // 8. Write a lambda expression to implement a lambda expression to calculate the factorial of a given number.
+    public static void factorial(Long n) {
+        LongUnaryOperator fac = num -> {
+            long result = 1;
+            for(long i = 1; i <= num; i++) {
+                result *= i;
+            }
+            return result;
+        };
+        long answer = fac.applyAsLong(n);
+        System.out.println(answer);
+    }
+
+    // 9. Write a Java program to implement a lambda expression to create a lambda expression to check if a number is prime.
 
 
 
@@ -85,14 +123,19 @@ public class LambdaProblems {
 
 
     public static void main(String[] args) {
+        List<String> input = Arrays.asList("gO", "DEvin", "UP uP");
+        List<Integer> nums = Arrays.asList(5, 2, 54, 654, 1, 3, 6, 0, 10);
+        List<Double> dubs = Arrays.asList(5.5, 2.2, 54.0, 654.11, 1.3, 3.22, 6.42, 0.4, 10.0);
+        List<Integer> listWithDups = Arrays.asList(1, 2, 3, 1, 4, 4, 56, 6, 56);
+
         LambdaProblems.sumTwoInts(10, 10);
         LambdaProblems.isStringEmpty("RAH");
-
-        List<String> input = Arrays.asList("gO", "DEvin", "UP uP");
         LambdaProblems.convertStringsToLower(input);
-
-        List<Integer> nums = Arrays.asList(5, 2, 54, 654, 1, 3, 6, 0, 10);
         LambdaProblems.filterEvensAndOdds(nums);
+        LambdaProblems.sortStrings(input);
+        LambdaProblems.averageOfDoubles(dubs);
+        LambdaProblems.removeDuplicates(listWithDups);
+        LambdaProblems.factorial(5L);
 
 
     }
